df8cd85523a6d5c8ee2375abfcffd57492c688f3
s-wei-h <59668682+s-wei-h@users.noreply.github.com>
2021-01-28 10:53:28 +0800

    commit 1

---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,31 @@
+﻿$("#submitButton").click( function () {
+    var assignmentPoints = parseFloat($('assignmentNum').value);
+    var projectPoints = parseFloat($('projectNum').value);
+    var quizPoints = parseFloat($('quizNum').value);
+    var examPoints = parseFloat($('examNum').value);
+    var intexPoints = parseFloat($('intexNum').value);
+    var total = (assignmentPoints * 0.5) +
+        (projectPoints * 0.1) +
+        (quizPoints * 0.1) +
+        (examPoints * 0.2) +
+        (intexPoints * 0.1);
+    var letterGrade;
+
+    if (total >= 90) {
+        letterGrade = 'A';
+    }
+    else if (total >= 80) {
+        letterGrade = 'B'
+    }
+    else if (total >= 70) {
+        letterGrade = 'C'
+    }
+    else if (total >= 60) {
+        letterGrade = 'D'
+    }
+    else {
+        letterGrade = 'F'
+    }
+
+    alert("Your total grade is: " + total.toString() + ". You recieved an " + letterGrade + " in this class");
+});
